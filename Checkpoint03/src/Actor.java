d2b17c2eadf3863aeb1ab467e475b2e7a7e45262
Augie <king.2656@buckeyemail.osu.edu>
2022-02-27 13:51:04 -0500

    Setup initial files

---
+++ Checkpoint03/src/Actor.java
@@ -0,0 +1,11 @@
+
+public class Actor {
+	public String name;
+	public String roleInMovie;
+	
+	public Actor(String NAME, String ROLEINMOVIE) {
+		name = NAME;
+		roleInMovie = ROLEINMOVIE;
+	}
+	
+}
