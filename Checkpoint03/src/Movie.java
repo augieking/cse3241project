f5d46f2c42af17468cb4d813b903c1f10f99f2fa
Augie <king.2656@buckeyemail.osu.edu>
2022-03-30 15:05:17 -0400

    fixed error with movie class

--- Checkpoint03/src/Movie.java
+++ Checkpoint03/src/Movie.java
@@ -4,24 +4,27 @@ public class Movie extends Entity {
     public String director;
     public String genre;
     public String title;
+    public int length;
     public ArrayList<Actor> actors;
 
-    public Movie(int YEAR, String DIRECTOR, String GENRE, String TITLE,
+    public Movie(int YEAR, String DIRECTOR, String GENRE, String TITLE, int LENGTH,
             ArrayList<Actor> ACTORS) {
         super(YEAR, "Movie");
         this.director = DIRECTOR;
         this.genre = GENRE;
         this.title = TITLE;
+        this.length = LENGTH;
         this.actors = ACTORS;
     }
 
-    public void update(int YEAR, String DIRECTOR, String GENRE, String TITLE,
+    public void update(int YEAR, String DIRECTOR, String GENRE, String TITLE, int LENGTH,
             ArrayList<Actor> ACTORS) {
 
         this.year = YEAR;
         this.director = DIRECTOR;
         this.genre = GENRE;
         this.title = TITLE;
+        this.length = LENGTH;
         this.actors = ACTORS;
     }
 
