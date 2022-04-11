886e35a568e1600621ae23c456ae3eb06c38322c
Augie <king.2656@buckeyemail.osu.edu>
2022-04-11 19:47:38 -0400

    Completed two useful queries

--- Checkpoint03/src/UsefulReports.java
+++ Checkpoint03/src/UsefulReports.java
@@ -9,7 +9,28 @@ import java.util.Scanner;
 
 public class UsefulReports {
 	
-	public static void displayCheckouts(Scanner input) {}
+	public static void displayCheckouts(Scanner input) {
+		System.out.print("Enter the email of the patron you want to see the number of checkouts for: ");
+		String email = input.nextLine();
+		
+		Connection conn = Database.c;
+        PreparedStatement stmt1 = null;
+        ResultSet results = null;
+        try {
+            String sql = "SELECT COUNT(*) "
+            		+ "FROM CUSTOMER_MEDIA_DUEDATE "
+            		+ "WHERE email = ?;";
+            stmt1 = conn.prepareStatement(sql);
+            stmt1.setString(1, email);
+            results = stmt1.executeQuery();
+            System.out.println("\nTOTAL NUMBER OF CHECKOUTS BY " + email + ": " + results.getString(1) + "\n");
+            stmt1.close();
+            results.close();
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
 	
 	public static void mostPopularActor(Scanner input) {}
 	
@@ -19,10 +40,39 @@ public class UsefulReports {
 	
 	public static void mostMoviesCheckedOut(Scanner input) {}
 	
-	public static void tracksByArtistBeforeYear(Scanner input) {}
+	public static void tracksByArtistBeforeYear(Scanner input) {
+		System.out.print("Enter the name of the ARTIST: ");
+		String artist = input.nextLine();
+		System.out.print("Enter the YEAR of which you want to find titles before (must be int): ");
+		int year = Integer.parseInt(input.nextLine());
+		
+		Connection conn = Database.c;
+        PreparedStatement stmt1 = null;
+        ResultSet results = null;
+        try {
+            String sql = "SELECT * "
+            		+ "FROM SONGWRITER, MUSIC "
+            		+ "WHERE SONGWRITER.artistName = ? AND SONGWRITER.musicId = MUSIC.id AND MUSIC.year < ?";
+            stmt1 = conn.prepareStatement(sql);
+            stmt1.setString(1, artist);
+            stmt1.setInt(2,  year);
+            results = stmt1.executeQuery();
+            
+            System.out.println();
+            while(results.next()) {
+            	System.out.println(results.getString("name"));
+            }
+            System.out.println();
+            stmt1.close();
+            results.close();
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+	}
 	
 	public static void usefulReports(Scanner input) {
-		System.out.println("A: Display all checkouts by a single patron.");
+		System.out.println("A: Display total number of checkouts by a single patron.");
 		System.out.println("B: Find the most popular ACTOR in the database.");
 		System.out.println("C: Find the most listened to ARTIST in the database.");
 		System.out.println("D: Find the most listened to AUTHOR in the database.");
